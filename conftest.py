4be7e2ced144df13379070255682a238ae41796a
Зубкова Яна <161482612+yanaz25@users.noreply.github.com>
2025-06-07 13:09:45 +0300

    небольшие правки

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action = 'store', default = "ru",
@@ -9,11 +9,12 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
 
     print("\nstart chrome browser for test..")
-    browser = webdriver.Chrome()
-    browser.get(link)
+    browser = webdriver.Chrome(options)
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
 
     yield browser
 
