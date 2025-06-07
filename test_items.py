a499046169747a5775e6f779d64e87a08d1cc576
Зубкова Яна <161482612+yanaz25@users.noreply.github.com>
2025-06-07 13:10:51 +0300

    небольшие правки

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 
 class TestItems():
     def test_add_button_displayed(self, browser):
+        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
         time.sleep(10)
         add_to_basket_button = browser.find_element(By.ID, "add_to_basket_form")
         assert add_to_basket_button.is_displayed()
